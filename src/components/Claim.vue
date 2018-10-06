55179bbf4b303ba6b63b1e8588ec6d362fbe5ed8
Jan Mager <jasiu.mager@gmail.com>
2018-10-06 13:29:06 +0200

    Adding all styles and components structure, emit

--- src/components/Claim.vue
+++ src/components/Claim.vue
@@ -17,20 +17,41 @@
     }
 </script>
 
-<style land="scss" scoped>
+<style lang="scss" scoped>
     .claimWrapper{
         display: flex;
         justify-content: center;
         align-items: center;
         flex-direction: column;
+        color: white;
     }
     .claim{
         font-size: 50px;
         margin: 0;
         font-weight: 800;
+
+        @media (min-width: 768px){
+            font-size: 70px;
+        }
+
+        @media (min-width: 1024px){
+            font-size: 100px;
+        }
+        
     }
     .subclaim{
         margin: 15px 0 0 0;
         font-size: 13px;
+        font-weight: 400px;
+        line-height: 125%;
+        max-width: 500px;
+
+        @media (min-width: 768px){
+            font-size: 14px;
+        }
+
+        @media (min-width: 1024px){
+            font-size: 16px;
+        }
     }
 </style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,34 +1,50 @@
 <template>
-    <div class="searchWrapper">
-        <input 
-          id="search" 
-          name="search" 
-          v-model="searchValue"
-          @input="handleInput"
-        />
-    </div>
+    <input 
+        id="search" 
+        name="search"
+        :value="value"
+        @input="handleChange"
+    />
 </template>
 
 <script>
 export default {
     name: 'SearchInput',
+    props: {
+        value: {
+            type: String,
+            required: true,
+        }
+    },
+    methods: {
+        handleChange(e){
+            this.$emit('input', e.target.value);
+        },
+    },
 }
 </script>
 
 <style lang="scss" scoped> 
-    .searchWrapper{
-        display: flex;
-        flex-direction: column;
-        width: 95%;
-        margin-top: 50px;
-        max-width: 300px;
+    input{
+            margin-top: 50px;
+            display: flex;
+            color: white;
+            text-align: center;
+            font-size: 18px;
+            font-family: Montserrat, sans-serif;    
+            height: 30px;
+            border: 0;
+            background: none;
+            border-bottom: 2px solid white;
+            transition: box-shadow ease-out .3s;
 
-        input{
-        height: 30px;
-        border: 0;
-        background: none;
-        border-bottom: 1px solid black;
+            @media (min-width: 1024px){
+                font-weight: 400px;
+            }
+    }
+
+    input:focus{
         outline: none;
-        }
-  }
+        box-shadow: 0 10px 20px -8px rgba(255,255,255,0.6);
+    }
 </style>
