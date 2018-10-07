c3c6a330c7e8a93a307ba321c98199192e8d2ec4
Jan Mager <jasiu.mager@gmail.com>
2018-10-07 16:27:40 +0200

    Added search result, css grid and animations (transiiotn in Vue)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="app">
-    <div class="wrapper">
-      <Background />
-      <Claim />
+    <div :class="[{ flexStart : step === 1 }, 'wrapper']">
+      <transition name="slide">
+        <img src="./assets/logo.png" class="logo" v-if="step === 1">
+      </transition>
+      <transition name="fade">
+        <Background v-if="step === 0" />
+      </transition>
+      <Claim v-if="step === 0" />
       <SearchInput 
         v-model="searchValue"
         @input="handleInput"
+        :dark="step === 1"
       />
+      <div class="results" v-if="results && !loading && step === 1">
+        <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id"/>
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +26,7 @@
   import Claim from '@/components/Claim.vue';
   import SearchInput from '@/components/SearchInput.vue';
   import Background from '@/components/Background.vue';
+  import Item from '@/components/Item.vue';
 
   const API = 'https://images-api.nasa.gov/search';
 
@@ -26,19 +36,25 @@
       Claim,
       SearchInput,
       Background,
+      Item
     },
     data(){
       return{
+        loading: false,
+        step: 0,
         searchValue: '',
         results: [],
       };
     },
     methods: {
       handleInput: debounce(function(){
+        this.loading = true;
         console.log(this.searchValue);
         axios.get(`${API}?q=${this.searchValue}&media_type=image`)
           .then((response) => {
             this.results = response.data.collection.items;
+            this.loading = false;
+            this.step = 1;
           })
           .catch((error) => {
             console.log(error);
@@ -62,18 +78,62 @@
     margin: 0;
     padding: 0;
   }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s ease;
+  }
+  .fade-enter, .fade-leave-to{
+    opacity: 0;
+  }
+  .slide-enter-active, .slide-leave-active {
+    transition: margin-top .3s ease;
+  }
+  .slide-enter, .slide-leave-to{
+    margin-top: -50px;
+  }
   .wrapper{
     display: flex;
+    position: relative;
     flex-direction: column;
     align-items: center;
     box-sizing: border-box;
     padding: 30px;
     margin: 0;
-    width: 100%;
+    min-width: 100%;
     min-height: 100vh;
     font-family: Montserrat, sans-serif;
     text-align: center;
     justify-content: center;
     align-items: center;
+
+    &.flexStart{
+      justify-content: flex-start;
+    }
+  }
+  .logo{
+    position: absolute;
+    top: 30px;
+    max-height: 20px;
+  }
+  .results{
+    width: 95%;
+    margin-top: 50px;
+    max-width: 1250px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 20px;
+    align-items: center;
+    justify-items: center;
+
+    @media(max-width: 1250px){
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    @media(max-width: 850px){
+      grid-template-columns: 1fr 1fr;
+    }
+
+    @media(max-width: 500px){
+      grid-template-columns: 1fr;
+    }
   }
 </style>
---
+++ src/components/Item.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :style="style" class="item" />
+</template>
+
+<script>
+export default {
+    name: 'Item',
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+    data(){
+        return{
+            photo: this.item.links[0].href,
+            title: this.item.data[0].title
+        };
+    },
+    computed: {
+        style(){
+            return `backgroundImage: url("${this.photo}")`;
+        }
+    }
+};
+</script>
+
+<style land="scss" scoped§>
+    .item{
+        width: 250px;
+        height: 250px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: 50%;
+    }
+</style>
